eb983b9a5041302439d9bfcad5662a6c9984d167
JaeSang Yoo <jsyoo5b@gmail.com>
2021-03-02 09:28:26 +0900

    feat: Add python3 solution using set
    Signed-off-by: JaeSang Yoo <jsyoo5b@gmail.com>

---
+++ BOJ/1764/set.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+if __name__ == '__main__':
+    cnt_n_heard, cnt_n_seen = map(int, input().split())
+    n_heards, n_seens = set(), set()
+    for i in range(cnt_n_heard):
+        n_heard = input()
+        n_heards.add(n_heard)
+    for i in range(cnt_n_seen):
+        n_seen = input()
+        n_seens.add(n_seen)
+
+    n_heard_seens = [ p for p in n_heards if p in n_seens ]
+    n_heard_seens.sort()
+    print(len(n_heard_seens))
+    for p in n_heard_seens:
+        print(p)
