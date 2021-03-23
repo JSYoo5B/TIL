af8fe5e18381678b9f6b6e61e254a255c47cb3bf
JaeSang Yoo <jsyoo5b@gmail.com>
2021-03-23 10:58:14 +0900

    feat: Implement hello world in each language
    Signed-off-by: JaeSang Yoo <jsyoo5b@gmail.com>

---
+++ Functional/Functhink/src/net/jsyoo5b/functhink/helloworld/HelloWorldScalaApp.scala
@@ -0,0 +1,7 @@
+package net.jsyoo5b.functhink.helloworld
+
+// Object which "extends App" acts as a main function
+// Called by App.delayedInit.body
+object HelloWorldScalaApp extends App {
+  println("HelloWorld from Scala App")
+}
