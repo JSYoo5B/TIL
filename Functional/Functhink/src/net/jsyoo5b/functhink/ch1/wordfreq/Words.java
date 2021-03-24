250f19d091c9e678bf5cb471038c352d7f9a4215
JaeSang Yoo <jsyoo5b@gmail.com>
2021-03-24 12:46:08 +0900

    refactor: Improve sample wordFreq
    Signed-off-by: JaeSang Yoo <jsyoo5b@gmail.com>

--- Functional/Functhink/src/net/jsyoo5b/functhink/ch1/wordfreq/Words.java
+++ Functional/Functhink/src/net/jsyoo5b/functhink/ch1/wordfreq/Words.java
@@ -5,25 +5,34 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Words {
-    private Set<String> NON_WORDS = new HashSet<String>() {{
+    // List-up non-words (articles, conjunctions, contraction followings, etc.)
+    private static final Set<String> NON_WORDS = new HashSet<String>() {{
         add("the"); add("and"); add("of");  add("to");  add("a");
         add("i");   add("it");  add("in");  add("or");  add("is");
         add("d"); add("s"); add("as");  add("so");  add("but");
         add("be");
     }};
 
-    public Map wordFreq(String words) {
+    public Map<String, Integer> wordFreq(String words) {
+        // TreeMap will store word and it's frequency <Word, FreqCnt>
         TreeMap<String, Integer> wordMap = new TreeMap<String, Integer>();
+        // Use Regex to find all words (alphabet groups)
         Matcher m = Pattern.compile("\\w+").matcher(words);
+
         while(m.find()) {
             String word = m.group().toLowerCase();
             if (! NON_WORDS.contains(word)) {
                 if (wordMap.get(word) == null) {
+                    // When the word weren't counted, add to entry
                     wordMap.put(word, 1);
                 }
                 else {
+                    // Otherwise, increase the count
                     wordMap.put(word, wordMap.get(word) + 1);
                 }
+
+                // TIP: if-else code block above can be shorten by:
+                //  wordMap.merge(word, 1, Integer::sum);
             }
         }
 
