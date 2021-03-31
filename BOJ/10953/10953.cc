183430fd0cc6d9bb3149313af0651c71d458ecb2
JaeSang Yoo <jsyoo5b@gmail.com>
2021-03-31 09:09:41 +0900

    feat: Add cpp solution
    Signed-off-by: JaeSang Yoo <jsyoo5b@gmail.com>

---
+++ BOJ/10953/10953.cc
@@ -0,0 +1,14 @@
+#include <cstdio>
+
+using namespace std;
+
+int main(void) {
+	int cases;
+	int a, b;
+
+	scanf("%d", &cases);
+	for (int i = 0; i < cases; i++) {
+		scanf("%d,%d", &a, &b);
+		printf("%d ", a + b);
+	}
+}
