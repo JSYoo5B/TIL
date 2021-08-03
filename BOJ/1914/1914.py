5ceb76ce0ce0d85bfbf8bdc7f9c575b4cb4f3225
JaeSang Yoo <jsyoo5b@gmail.com>
2021-08-03 10:34:06 +0900

    feat: Add python3 solution
    * Problem 1914 is an extension of 11729
    
    Signed-off-by: JaeSang Yoo <jsyoo5b@gmail.com>

---
+++ BOJ/1914/1914.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+def hanoi_tower(level, src, dst, tmp):
+    if level == 0:
+        return
+    # Move upper disks to temporal pole
+    hanoi_tower(level-1, src, tmp, dst)
+    # Move actual disk
+    print(src, dst)
+    # Move upper disks from temporal to destination pole
+    hanoi_tower(level-1, tmp, dst, src)
+
+if __name__ == '__main__':
+    disks = int(input())
+
+    # Known hanoi tower steps formula
+    total_count = 2 ** disks - 1
+    print(total_count)
+    
+    if (disks <= 20):
+        hanoi_tower(disks, 1, 3, 2)
