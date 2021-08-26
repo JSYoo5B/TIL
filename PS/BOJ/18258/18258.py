5551053cb649daa70fca47d87c7af6ce5834511f
JaeSang Yoo <jsyoo5b@gmail.com>
2021-08-26 23:34:56 +0900

    feat: Add python3 solution
    * Time limit exceeds, need to optimize IO
    
    Signed-off-by: JaeSang Yoo <jsyoo5b@gmail.com>

---
+++ PS/BOJ/18258/18258.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+from collections import deque
+
+if __name__ == '__main__':
+    op_cnt = int(input())
+    q = deque()
+    for _ in range(op_cnt):
+        operation = input()
+        arg = 0
+        if operation.startswith('push'):
+            arg = operation.split()[1]
+            operation = operation.split()[0]
+
+        if operation == 'push':
+            q.append(arg)
+        elif operation == 'pop':
+            result = q.popleft() if len(q) > 0 else -1
+            print(result)
+        elif operation == 'size':
+            result = len(q)
+            print(result)
+        elif operation == 'empty':
+            result = 1 if len(q) == 0 else 0
+            print(result)
+        elif operation == 'front':
+            result = q[0] if len(q) > 0 else -1
+            print(result)
+        elif operation == 'back':
+            result = q[-1] if len(q) > 0 else -1
+            print(result)
