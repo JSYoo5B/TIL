b1da6a141f39af4ba58eb016c37f19a3c9615006
JaeSang Yoo <jsyoo5b@gmail.com>
2021-09-05 12:38:38 +0900

    feat: Add python3 solution
    Signed-off-by: JaeSang Yoo <jsyoo5b@gmail.com>

---
+++ PS/BOJ/3053/3053.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+if __name__ == '__main__':
+    r = int(input())
+    PI = 3.14159265359
+    euclide_circle = r * r * PI
+    taxi_circle = r * r * 2
+
+    print(euclide_circle)
+    print(taxi_circle)
