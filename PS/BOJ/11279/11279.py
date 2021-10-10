e4659c1421f2b99ebe45d55db67a6684a11c6b58
JaeSang Yoo <jsyoo5b@gmail.com>
2021-10-10 13:16:35 +0900

    feat: Add python3 solution
    Signed-off-by: JaeSang Yoo <jsyoo5b@gmail.com>

---
+++ PS/BOJ/11279/11279.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import heapq
+import sys
+
+input = sys.stdin.readline
+
+if __name__ == '__main__':
+    operations_cnt = int(input())
+    heap = []
+    for _ in range(operations_cnt):
+        operation = int(input())
+        if operation == 0:
+            answer = 0
+            if len(heap) > 0:
+                answer = heapq.heappop(heap)
+                answer *= -1
+            print(answer)
+        else:
+            heapq.heappush(heap, -1 * operation)
+            
