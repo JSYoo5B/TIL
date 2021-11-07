0635521da9f2e6b14970dd1a58e2610f9253cddd
JaeSang Yoo <jsyoo5b@gmail.com>
2021-11-07 23:26:28 +0900

    feat: Modify malloc.c to invoke syscall
    Signed-off-by: JaeSang Yoo <jsyoo5b@gmail.com>

--- Kernel/syscall/malloc.c
+++ Kernel/syscall/malloc.c
@@ -1,7 +1,11 @@
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 int main(void)
 {
-	void* addr = malloc(sizeof(int));
+	for (int i = 0; i < 4; i++) {
+		char* addr = (char*)malloc(1024 * 1024);
+	}
 	return 0;
 }
