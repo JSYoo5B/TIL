3b4080a3aaa47143c853ea5ccfe03de918b89a81
JaeSang Yoo <jsyoo5b@gmail.com>
2021-11-30 11:59:03 +0900

    feat: Add python3 solution
    Signed-off-by: JaeSang Yoo <jsyoo5b@gmail.com>

---
+++ PS/BOJ/2623/2623.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+def topological_sort(graph, in_degrees):
+    nodes_cnt = len(in_degrees)
+    topo_sort = []
+
+    que = deque([ i for i in range(1, nodes_cnt) if in_degrees[i] == 0 ])
+    while len(que) > 0:
+        current = que.popleft()
+        topo_sort.append(current)
+        for n in graph[current]:
+            in_degrees[n] -= 1
+            if in_degrees[n] == 0:
+                que.append(n)
+    if len(topo_sort) < nodes_cnt - 1:
+        topo_sort = []
+    return topo_sort
+
+
+if __name__ == '__main__':
+    nodes_cnt, graph_cnt = map(int, input().split())
+    in_degrees = [ 0 for _ in range(nodes_cnt + 1) ]
+    graph = [ [] for _ in range(nodes_cnt + 1) ]
+    for _ in range(graph_cnt):
+        graph_info = list(map(int, input().split()))
+        for i in range(1, graph_info[0]):
+            src, dst = graph_info[i], graph_info[i+1]
+            graph[src].append(dst)
+            in_degrees[dst] += 1
+    
+    linear_order = topological_sort(graph, in_degrees)
+    for n in linear_order:
+        print(n)
+    if len(linear_order) == 0:
+        print(0)
