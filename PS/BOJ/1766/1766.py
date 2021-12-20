c9dd5b608a0e7997e046aeb4b29180f3b1ef6b60
JaeSang Yoo <jsyoo5b@gmail.com>
2021-12-20 10:19:34 +0900

    feat: Add python3 solution
    Signed-off-by: JaeSang Yoo <jsyoo5b@gmail.com>

---
+++ PS/BOJ/1766/1766.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+from heapq import heappush, heappop
+import sys
+input = sys.stdin.readline
+
+def topological_sort(graph, in_degrees):
+    nodes_cnt = len(in_degrees)
+    topo_sort = []
+
+    heap = []
+    for i in range(1, nodes_cnt):
+        if in_degrees[i] == 0:
+            heappush(heap, i)        
+    while len(heap) > 0:
+        current = heappop(heap)
+        topo_sort.append(current)
+        for n in graph[current]:
+            in_degrees[n] -= 1
+            if in_degrees[n] == 0:
+                heappush(heap, n)
+    if len(topo_sort) < nodes_cnt - 1:
+        topo_sort = []
+    return topo_sort
+
+
+if __name__ == '__main__':
+    nodes_cnt, graph_cnt = map(int, input().split())
+    in_degrees = [ 0 for _ in range(nodes_cnt + 1) ]
+    graph = [ [] for _ in range(nodes_cnt + 1) ]
+    for _ in range(graph_cnt):
+        src, dst = map(int, input().split())
+        graph[src].append(dst)
+        in_degrees[dst] += 1
+
+    linear_order = topological_sort(graph, in_degrees)
+    print(*linear_order)
