ca4bb49c276219ca04641a083487dfcccc970e72
JaeSang Yoo <jsyoo5b@gmail.com>
2025-02-23 18:47:19 +0900

    TDDbE(ch04): Prepare for changing amount to private

--- TDD/TDD-by-example/money/dollar_test.go
+++ TDD/TDD-by-example/money/dollar_test.go
@@ -1,12 +1,13 @@
-package money
+package money_test
 
 import (
 	"github.com/stretchr/testify/assert"
+	"tddbe/money"
 	"testing"
 )
 
 func TestMultiplication(t *testing.T) {
-	five := NewDollar(5)
+	five := money.NewDollar(5)
 
 	product := five.Times(2)
 	assert.Equal(t, 10, product.Amount)
@@ -16,9 +17,19 @@ func TestMultiplication(t *testing.T) {
 }
 
 func TestEquality(t *testing.T) {
-	assert.True(t, *NewDollar(5) == *NewDollar(5))
-	assert.False(t, *NewDollar(5) == *NewDollar(6))
+	t.Run("pointer compare and deep compare", func(t *testing.T) {
+		assert.False(t, money.NewDollar(5) == money.NewDollar(5))
+		assert.True(t, *money.NewDollar(5) == *money.NewDollar(5))
+		assert.False(t, *money.NewDollar(5) == *money.NewDollar(6))
+	})
 
-	assert.True(t, NewDollar(5).Equals(NewDollar(5)))
-	assert.False(t, NewDollar(5).Equals(NewDollar(6)))
+	t.Run("compare by methods", func(t *testing.T) {
+		assert.True(t, money.NewDollar(5).Equals(money.NewDollar(5)))
+		assert.False(t, money.NewDollar(5).Equals(money.NewDollar(6)))
+	})
+
+	t.Run("testify supports deep compare", func(t *testing.T) {
+		assert.Equal(t, money.NewDollar(5), money.NewDollar(5))
+		assert.NotEqual(t, money.NewDollar(5), money.NewDollar(6))
+	})
 }
