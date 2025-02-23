23d73ed690857d2f7db63931acf7cac832512c5e
JaeSang Yoo <jsyoo5b@gmail.com>
2025-02-23 19:51:38 +0900

    TDDbE(ch06): Add franc equality tests

--- TDD/TDD-by-example/money/franc_test.go
+++ TDD/TDD-by-example/money/franc_test.go
@@ -12,3 +12,15 @@ func TestFrancMultiplication(t *testing.T) {
 	assert.Equal(t, money.NewFranc(10), five.Times(2))
 	assert.Equal(t, money.NewFranc(15), five.Times(3))
 }
+
+func TestFrancEquality(t *testing.T) {
+	t.Run("compare by methods", func(t *testing.T) {
+		assert.True(t, money.NewFranc(5).Equals(money.NewFranc(5)))
+		assert.False(t, money.NewFranc(5).Equals(money.NewFranc(6)))
+	})
+
+	t.Run("testify supports deep compare", func(t *testing.T) {
+		assert.Equal(t, money.NewFranc(5), money.NewFranc(5))
+		assert.NotEqual(t, money.NewFranc(5), money.NewFranc(6))
+	})
+}
