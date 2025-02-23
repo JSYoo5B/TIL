be5f50210cf90700b0eaaa26a44842e55e5f36b6
JaeSang Yoo <jsyoo5b@gmail.com>
2025-02-23 19:42:00 +0900

    TDDbE(ch06): Extract common part to Money

--- TDD/TDD-by-example/money/dollar.go
+++ TDD/TDD-by-example/money/dollar.go
@@ -1,16 +1,18 @@
 package money
 
 type Dollar struct {
-	amount int
+	Money
 }
 
 func (d *Dollar) Times(multiplier int) *Dollar {
-	return &Dollar{amount: d.amount * multiplier}
+	return NewDollar(d.amount * multiplier)
 }
 func (d *Dollar) Equals(other any) bool {
 	otherDollar, ok := other.(*Dollar)
 	return ok && d.amount == otherDollar.amount
 }
 func NewDollar(amount int) *Dollar {
-	return &Dollar{amount: amount}
+	return &Dollar{
+		Money: Money{amount: amount},
+	}
 }
--- TDD/TDD-by-example/money/franc.go
+++ TDD/TDD-by-example/money/franc.go
@@ -1,16 +1,18 @@
 package money
 
 type Franc struct {
-	amount int
+	Money
 }
 
 func (f *Franc) Times(multiplier int) *Franc {
-	return &Franc{amount: f.amount * multiplier}
+	return NewFranc(f.amount * multiplier)
 }
 func (f *Franc) Equals(other any) bool {
 	otherFranc, ok := other.(*Franc)
 	return ok && f.amount == otherFranc.amount
 }
 func NewFranc(amount int) *Franc {
-	return &Franc{amount: amount}
+	return &Franc{
+		Money: Money{amount},
+	}
 }
